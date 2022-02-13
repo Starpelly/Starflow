fb3c22f4268a0f36c2e4776ba62b71154efac13e
Braedon <braedonwilcox@gmail.com>
2022-02-12 23:23:58 -0500

    Create Debug.cs

---
+++ Tickflow.Framework/Debug.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tickflow
+{
+    public class Debug
+    {
+        public static void Log(object message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
